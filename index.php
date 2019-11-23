0478a4b7cdda5d05acdd69129389fda00c52da70
MaverickEsq <sloth@faggotry.org>
2019-11-24 03:32:21 +1030

    Tempal

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+if (!file_exists('./inc/paste.db')) {
+	$db = new SQLite3('./inc/paste.db');
+	$db->exec("CREATE TABLE pastes(pid TEXT, data BLOB, ip TEXT)");
+	$db->close;
+}
+
+$config = parse_ini_file('config.ini');
+ini_set('post_max_size', $config['max_size']);
+
+//maybe only run this when we need it
+$db = new SQLite3('./inc/paste.db');
+
+if (isset($_POST[$config['name']])) {
+	$db = new SQLite3('./inc/paste.db');
+	$idchars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$pasteid = substr(str_shuffle($idchars), 0, 8);
+
+	$db->exec('INSERT INTO pastes(pid, data, ip) VALUES(\'' . $pasteid . '\', \'' . SQLite3::escapeString($_POST[$config['name']]) . '\', \'' . $_SERVER['REMOTE_ADDR'] . '\')');
+
+	print 'https://' . $config['url'] . '/' . $pasteid;
+	$db->close;
+	die;
+} else if (isset($_GET['id'])) {
+	//get paste
+	$db = new SQLite3('./inc/paste.db');
+	$paste = $db->querySingle('SELECT data FROM pastes WHERE pid=\'' . SQLite3::escapeString($_GET['id']) . '\'');
+	$db->close;
+	if ($paste == NULL) {
+		die("No such paste found");
+	}
+
+	if (isset($_GET['hl'])) {
+		//highlighting
+		include_once './inc/geshi.php';
+		//null for the path cause we leave it default, true to
+		//return it instead of echoing it.
+		$paste = geshi_highlight($paste, $_GET['hl'], null, true);
+	}
+	print $paste;
+	die;
+}
+
+print '<html ml-update="aware"><head><title>' . $config['name'] . '</title></head><body>
+        
+<style> a { text-decoration: none } </style>
+<pre>' . $config['name'] . '(1)                          ' . strtoupper($config['name']) . '                          ' . $config['name'] . '(1)
+
+NAME
+    sprunge: command line pastebin.
+
+SYNOPSIS
+    <code>&lt;command&gt; | curl -F \'' . $config['name'] . '=<-\' http://' . $config['url'] . '</code>
+
+DESCRIPTION
+    add <b>?&lt;lang&gt;</b> to resulting url for line numbers and syntax highlighting
+    use <a onclick="document.getElementById(\'paste\').style.display = \'block\'" href="#">this form</a> to paste from a browser
+    <div id="paste" style="display:none;"><form action="http://' . $config['url'] . '" method="POST" accept-charset="UTF-8"><textarea name="' . $config['name'] . '" cols="80" rows="24"></textarea><br><button type="submit">' . $config['name'] . '</button></form></div>
+EXAMPLES
+    ~$ cat crash/bang | curl -F \'' . $config['name'] . '=<-\' http://' . $config['url'] . '
+       http://' . $config['url'] . '/aXZI
+    ~$ firefox http://' . $config['url'] . '/aXZI?py
+
+SEE ALSO
+    http://github.com/MaverickEsq/tempal
+
+</pre>
+</body>
+</html>';

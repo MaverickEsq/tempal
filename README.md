37c4b9e2b4a9a29d7f8654f46d239cd5abce748f
MaverickEsq <sloth@faggotry.org>
2019-11-24 04:57:00 +1030

    bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# tempal
+       _____                          _
+      |_   _|__ _ __ ___  _ __   __ _| |
+        | |/ _ \ '_ ` _ \| '_ \ / _` | |
+        | |  __/ | | | | | |_) | (_| | |
+        |_|\___|_| |_| |_| .__/ \__,_|_|
+                         |_|
+	     Where fleetingness is a virtue
+       
+## Description
+Inspired by *sprunge*, *ix.io* and others, I decided to make a small, command line pastebin. The things I saw when looking at other pastebins were that they used things like weird google storage APIs and didn't have a web form that could be quickly used.
+
+To this end, I decided to make a pastebin that met a few criteria.
+1. It had to be portable, not requiring any installs or
+	setups and could be dragged and dropped.
+2. It had to use a local database. Tinfoil hat compatibility.
+3. It needed a webform, others had web forms that required
+	pasting things into the address bar.
+4. It should meet the alternatives in terms of functionality.
+
+So, tempal was written in php, using an sqlite3 database and as little complexity as possible.
+
+## Deployment
+Simply `git clone` or extract from zip into a web directory. It expects to be at the root directory, theres probably a simple htaccess fix for that. The first time the script runs it will make a database.
+
+## Credits
+This uses GeSHi (http://qbnz.com/highlighter/) but for simplicity's sake I have included it.
+
+Credit obviously to sprunge.us and ix.io who inspire this and whose main page I shamelessly ripped off
+
+`mpan` probably deserves a credit, not for input to this project but just in general.
+
+## License
+Tempal is licensed under the WTFPL under the condition that any fork or derative work remains under the WTFPL.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+	 Copyright (C) 2013 Sloth <sloth@faggotry.org>
+
+	 Everyone is permitted to copy and distribute verbatim or modified
+	 copies of this license document, and changing it is allowed as long
+	 as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+     TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+    0. You just DO WHAT THE FUCK YOU WANT TO.
--- index.php
+++ index.php
@@ -12,42 +12,44 @@ ini_set('post_max_size', $config['max_size']);
 $db = new SQLite3('./inc/paste.db');
 
 if (isset($_POST[$config['name']])) {
-	$db = new SQLite3('./inc/paste.db');
-	$idchars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$pasteid = substr(str_shuffle($idchars), 0, 8);
+        $db = new SQLite3('./inc/paste.db');
+        $idchars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $pasteid = substr(str_shuffle($idchars), 0, 8);
 
-	$db->exec('INSERT INTO pastes(pid, data, ip) VALUES(\'' . $pasteid . '\', \'' . SQLite3::escapeString($_POST[$config['name']]) . '\', \'' . $_SERVER['REMOTE_ADDR'] . '\')');
+        $db->exec('INSERT INTO pastes(pid, data, ip) VALUES(\'' . $pasteid . '\', \'' . SQLite3::escapeString($_POST[$config['name']]) . '\', \'' . $_SERVER['REMOTE_ADDR'] . '\')');
 
-	print 'https://' . $config['url'] . '/' . $pasteid;
-	$db->close;
-	die;
+        print 'https://' . $config['url'] . '/' . $pasteid . "\n";
+        $db->close;
+        die;
 } else if (isset($_GET['id'])) {
-	//get paste
-	$db = new SQLite3('./inc/paste.db');
-	$paste = $db->querySingle('SELECT data FROM pastes WHERE pid=\'' . SQLite3::escapeString($_GET['id']) . '\'');
-	$db->close;
-	if ($paste == NULL) {
-		die("No such paste found");
-	}
+        //get paste
+        $db = new SQLite3('./inc/paste.db');
+        $paste = $db->querySingle('SELECT data FROM pastes WHERE pid=\'' . SQLite3::escapeString($_GET['id']) . '\'');
+        $db->close;
+        if ($paste == NULL) {
+                die("No such paste found");
+        }
 
-	if (isset($_GET['hl'])) {
-		//highlighting
-		include_once './inc/geshi.php';
-		//null for the path cause we leave it default, true to
-		//return it instead of echoing it.
-		$paste = geshi_highlight($paste, $_GET['hl'], null, true);
-	}
-	print $paste;
-	die;
+        if (isset($_GET['hl'])) {
+                //highlighting
+                include_once './inc/geshi.php';
+                //null for the path cause we leave it default, true to
+                //return it instead of echoing it.
+                $paste = geshi_highlight($paste, $_GET['hl'], null, true);
+        } else {
+                header("Content-Type: text/plain");
+        }
+        print $paste;
+        die;
 }
 
 print '<html ml-update="aware"><head><title>' . $config['name'] . '</title></head><body>
-        
+
 <style> a { text-decoration: none } </style>
 <pre>' . $config['name'] . '(1)                          ' . strtoupper($config['name']) . '                          ' . $config['name'] . '(1)
 
 NAME
-    sprunge: command line pastebin.
+    ' . $config['name'] . ': command line pastebin.
 
 SYNOPSIS
     <code>&lt;command&gt; | curl -F \'' . $config['name'] . '=<-\' http://' . $config['url'] . '</code>

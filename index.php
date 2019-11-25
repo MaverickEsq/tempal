65aa7b92ee83a88f9d4738cec1b57f8ca1a78278
MaverickEsq <sloth@faggotry.org>
2019-11-26 05:26:57 +1030

    lexers, commenting, minimizing

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Make the database if its not there
 if (!file_exists('./inc/paste.db')) {
 	$db = new SQLite3('./inc/paste.db');
 	$db->exec("CREATE TABLE pastes(pid TEXT, data BLOB, ip TEXT)");
@@ -8,16 +9,17 @@ if (!file_exists('./inc/paste.db')) {
 $config = parse_ini_file('config.ini');
 ini_set('post_max_size', $config['max_size']);
 
-//maybe only run this when we need it
-$db = new SQLite3('./inc/paste.db');
-
 if (isset($_POST[$config['name']])) {
         $db = new SQLite3('./inc/paste.db');
+
+        // You would need about 435 exabytes of pastes before an 8 char ID
+        // will definitely repeat. Good enough for me.
         $idchars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $pasteid = substr(str_shuffle($idchars), 0, 8);
 
         $db->exec('INSERT INTO pastes(pid, data, ip) VALUES(\'' . $pasteid . '\', \'' . SQLite3::escapeString($_POST[$config['name']]) . '\', \'' . $_SERVER['REMOTE_ADDR'] . '\')');
 
+        header("Content-Type: text/plain");
         print 'https://' . $config['url'] . '/' . $pasteid . "\n";
         $db->close();
         die;
@@ -27,15 +29,24 @@ if (isset($_POST[$config['name']])) {
         $paste = $db->querySingle('SELECT data FROM pastes WHERE pid=\'' . SQLite3::escapeString($_GET['id']) . '\'');
         $db->close();
         if ($paste == NULL) {
-                die("No such paste found");
+        	header("Content-Type: text/plain");
+            die("No such paste found");
         }
 
         if (isset($_GET['hl'])) {
                 //highlighting
                 include_once './inc/geshi.php';
+
+                // Get language returns "text" if its not a file extension
+                // This lets us use 'pl' or 'perl' to highligh a block
+                if (GeSHi::get_language_name_from_extension(strtolower($_GET['hl'])) != 'text') {
+                        $lexer = GeSHi::get_language_name_from_extension(strtolower($_GET['hl']));
+                } else {
+                        $lexer = $_GET['hl'];
+                }
                 //null for the path cause we leave it default, true to
                 //return it instead of echoing it.
-                $paste = geshi_highlight($paste, $_GET['hl'], null, true);
+                $paste = geshi_highlight($paste, $lexer, null, true);
         } else {
                 header("Content-Type: text/plain");
         }
@@ -43,6 +54,7 @@ if (isset($_POST[$config['name']])) {
         die;
 }
 
+//Printing the page. If we do things, it dies, so it never gets here
 print '<html ml-update="aware"><head><title>' . $config['name'] . '</title></head><body>
 
 <style> a { text-decoration: none } </style>
